b7e89ab80f07228d60854700562fa78555bb6c13
github username <hector.bale11@gmail.com>
2020-11-06 14:16:52 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+tuesdayLunch = Lunch.new(roman, chicken, water)
+
+
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = dessert;
+    }
+}
+
+mondayDinner = Dinner.new(cobb, cheese, steak, cake)
